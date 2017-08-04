04592c6212b12e3f39a9e360ba7810b459481532
CharDale Barnes <chbarnes@hayneedle.com>
2017-08-04 10:36:06 -0500

    Video image thumbnail and titles are now being pulled in.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
-};
+// const VideoListItem = (props) => {
+//     // pull the video from the props object, set as a variable:
+//     const video = props.video;
+    
+//     return <li>Video</li>
+// };
+// or you can just do: 
+//  which says, 'this argument has a property called video, please assign it to a const called video:
+const VideoListItem = ({video}) => {
+    // pull the image url from the video object for use as thumbnail:
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return <li className="list-group-item">
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" src={imageUrl} />
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+    </li>
 
+};
 export default VideoListItem;
